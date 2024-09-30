ce06a559c1af6e60b08bc4f531453d572e8608bc
workComputerWSL <lwt6077@gmail.com>
2024-09-30 17:34:26 +0800

    cursor plugins
    Signed-off-by: workComputerWSL <lwt6077@gmail.com>

---
+++ lua/plugins/fast-cursor-move.nvim.lua
@@ -0,0 +1,9 @@
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+-- Customize fast-cursor-move.nvim
+
+-- @type LazySpec
+
+return {
+  "xiyaowong/fast-cursor-move.nvim",
+}
