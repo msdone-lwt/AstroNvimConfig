1a667774347f8080dc505870c5f42444c02a27c2
workComputerWSL <lwt6077@gmail.com>
2024-10-25 17:51:51 +0800

    Change the model to gpt-4o
    Signed-off-by: workComputerWSL <lwt6077@gmail.com>

--- lua/plugins/avante.nvim.lua
+++ lua/plugins/avante.nvim.lua
@@ -15,9 +15,20 @@ return {
     -- NOTE: copilot
     provider = "copilot", -- Recommend using Claude
     auto_suggestions_provider = "copilot", -- Since auto-suggestions are a high-frequency operation and therefore expensive, it is recommended to specify an inexpensive provider or even a free provider: copilot
+    ---@type AvanteSupportedProvider
+    copilot = {
+      -- endpoint = "https://api.githubcopilot.com",
+      model = "gpt-4o",
+      -- proxy = nil, -- [protocol://]host[:port] Use this proxy
+      -- allow_insecure = false, -- Allow insecure server connections
+      -- timeout = 30000, -- Timeout in milliseconds
+      -- temperature = 0,
+      -- max_tokens = 4096,
+    },
     -- NOTE: openai
     -- provider = "openai", -- Recommend using Claude
     -- auto_suggestions_provider = "openai", -- Since auto-suggestions are a high-frequency operation and therefore expensive, it is recommended to specify an inexpensive provider or even a free provider: copilot
+    ---@type AvanteSupportedProvider
     -- openai = {
     --   endpoint = "https://burn.hair/v1",
     --   model = "gpt-4o",
