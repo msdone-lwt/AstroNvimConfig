38b68f54ac9aece58c99051ed4d24def3bbc26bd
workComputerWSL <lwt6077@gmail.com>
2025-06-20 17:36:17 +0800

    astro：取消 virtual_line
    Signed-off-by: workComputerWSL <lwt6077@gmail.com>

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -15,7 +15,7 @@ return {
       large_buf = { size = 1024 * 500, lines = 10000 }, -- set global limits for large files for disabling features like treesitter
       autopairs = true, -- enable autopairs at start
       cmp = true, -- enable completion at start
-      diagnostics = { virtual_text = false, virtual_lines = true, underline = false }, -- diagnostic settings on startup
+      diagnostics = { virtual_text = true, virtual_lines = false, underline = true }, -- diagnostic settings on startup
       highlighturl = true, -- highlight URLs at start
       notifications = true, -- enable notifications at start
     },
